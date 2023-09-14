69a955c116598b62e373fcdb8defdc00a59caa63
Pcosby5 <pcosby50@gmail.com>
2023-09-14 14:01:08 +0000

    solved merged issues

--- commands.c
+++ commands.c
@@ -5,10 +5,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-<<<<<<< HEAD
-=======
-
->>>>>>> 7b438768a8b9bca30c718c10910b47dd95cab9e7
 #define MAX_INPUT_LENGTH 100
 
 /**
@@ -18,80 +14,44 @@
  *
  * Return: Full path to the executable, or NULL if not found.
  */
-<<<<<<< HEAD
-
 char *findExecutable(char *command)
 {
     char *path = getenv("PATH");
     char *path_copy = strdup(path);
     char *token;
+
     if (path_copy == NULL)
     {
         perror("Error duplicating PATH");
         exit(EXIT_FAILURE);
     }
+
     /* can use this func: token = strtok(path_copy, ":"); */
     token = customTokenize(path_copy, ":", NULL);
     while (token != NULL)
     {
         char *full_path = (char *)malloc(strlen(token) + strlen(command) + 2);
+
         if (full_path == NULL)
         {
             perror("Error allocating memory");
             free(path_copy);
             exit(EXIT_FAILURE);
         }
+
         sprintf(full_path, "%s/%s", token, command);
         if (access(full_path, X_OK) == 0)
         {
             free(path_copy);
             return (full_path);
         }
+
         free(full_path);
         token = customTokenize(path_copy, ":", NULL);
     }
+
     free(path_copy);
     return (NULL);
-=======
-char *findExecutable(char *command)
-{
-	char *path = getenv("PATH");
-	char *path_copy = strdup(path);
-	char *token;
-
-	if (path_copy == NULL)
-	{
-		perror("Error duplicating PATH");
-		exit(EXIT_FAILURE);
-	}
-
-	/* can use this func: token = strtok(path_copy, ":"); */
-	token = customTokenize(path_copy, ":", NULL);
-	while (token != NULL)
-	{
-		char *full_path = (char *)malloc(strlen(token) + strlen(command) + 2);
-
-		if (full_path == NULL)
-		{
-			perror("Error allocating memory");
-			free(path_copy);
-			exit(EXIT_FAILURE);
-		}
-
-		sprintf(full_path, "%s/%s", token, command);
-		if (access(full_path, X_OK) == 0)
-		{
-			free(path_copy);
-			return (full_path);
-		}
-
-		free(full_path);
-		token = customTokenize(path_copy, ":", NULL);
-	}
-
-	free(path_copy);
-	return (NULL);
->>>>>>> 7b438768a8b9bca30c718c10910b47dd95cab9e7
 }
 
 /**
@@ -101,31 +61,17 @@ char *findExecutable(char *command)
  * @commands: An array to store the individual commands.
  * @num_commands: Pointer to the number of commands found.
  */
-<<<<<<< HEAD
-
 void tokenizeInput(char *input, char *commands[], size_t *num_commands)
 {
     char *token;
     *num_commands = 0;
     token = strtok(input, ";");
+
     while (token != NULL && *num_commands < (MAX_INPUT_LENGTH / 2))
     {
         commands[(*num_commands)++] = token;
         token = strtok(NULL, ";");
     }
-=======
-void tokenizeInput(char *input, char *commands[], size_t *num_commands)
-{
-	char *token;
-	*num_commands = 0;
-	token = strtok(input, ";");
-
-	while (token != NULL && *num_commands < (MAX_INPUT_LENGTH / 2))
-	{
-		commands[(*num_commands)++] = token;
-		token = strtok(NULL, ";");
-	}
->>>>>>> 7b438768a8b9bca30c718c10910b47dd95cab9e7
 }
 
 /**
@@ -134,17 +80,18 @@ void tokenizeInput(char *input, char *commands[], size_t *num_commands)
  * @commands: An array of commands to execute.
  * @num_commands: The number of commands in the array.
  */
-<<<<<<< HEAD
-
 void executeCommands(char *commands[], size_t num_commands)
 {
     size_t i;
+
     for (i = 0; i < num_commands; i++)
     {
         char *command = commands[i];
+
         if (strncmp(command, "exit", 4) == 0)
         {
             int status = 0;
+
             if (sscanf(command + 4, " %d", &status) == 1)
                 handleExit(status);
             else
@@ -159,6 +106,7 @@ void executeCommands(char *commands[], size_t num_commands)
         else
         {
             pid_t child_pid = fork();
+
             if (child_pid == -1)
             {
                 perror("Error forking");
@@ -173,49 +121,6 @@ void executeCommands(char *commands[], size_t num_commands)
                 handleParentProcess(child_pid);
         }
     }
-=======
-void executeCommands(char *commands[], size_t num_commands)
-{
-	size_t i;
-
-	for (i = 0; i < num_commands; i++)
-	{
-		char *command = commands[i];
-
-		if (strncmp(command, "exit", 4) == 0)
-		{
-			int status = 0;
-
-			if (sscanf(command + 4, " %d", &status) == 1)
-				handleExit(status);
-			else
-				handleExit(0);
-		}
-		else if (strncmp(command, "setenv", 6) == 0)
-			handleSetenvCommand(command);
-		else if (strncmp(command, "unsetenv", 8) == 0)
-			handleUnsetenvCommand(command);
-		else if (strncmp(command, "cd", 2) == 0)
-			handleCdCommand(command);
-		else
-		{
-			pid_t child_pid = fork();
-
-			if (child_pid == -1)
-			{
-				perror("Error forking");
-				exit(EXIT_FAILURE);
-			}
-			if (child_pid == 0)
-			{
-				handleChildProcess(command);
-				exit(EXIT_SUCCESS);
-			}
-			else
-				handleParentProcess(child_pid);
-		}
-	}
->>>>>>> 7b438768a8b9bca30c718c10910b47dd95cab9e7
 }
 
 /**
@@ -224,28 +129,12 @@ void executeCommands(char *commands[], size_t num_commands)
  *
  * @input: The input string containing multiple commands.
  */
-<<<<<<< HEAD
-
 void processCommands(char *input)
 {
     char *commands[MAX_INPUT_LENGTH / 2];
     size_t num_commands = 0;
+
     tokenizeInput(input, commands, &num_commands);
+
     executeCommands(commands, num_commands);
 }
-=======
-void processCommands(char *input)
-{
-	char *commands[MAX_INPUT_LENGTH / 2];
-	size_t num_commands = 0;
-
-	tokenizeInput(input, commands, &num_commands);
-
-	executeCommands(commands, num_commands);
-}
-
-
-
-
-
->>>>>>> 7b438768a8b9bca30c718c10910b47dd95cab9e7
