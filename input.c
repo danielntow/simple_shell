c1948d1ade5c4fbaf74cc9101c2a2da33df09569
danielntow <danielntow@gmail.com>
2023-09-15 05:09:16 +0000

    done

--- input.c
+++ input.c
@@ -11,10 +11,7 @@
 
 #define BUFFER_SIZE 1024 /* Adjust the buffer size as needed */
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 7b438768a8b9bca30c718c10910b47dd95cab9e7
 /**
  * readInput - Read user input.
  *
@@ -23,33 +20,7 @@
  *
  * Return: 0 on success, -1 on failure.
  */
-<<<<<<< HEAD
 
-int readInput(char input[], size_t *input_length)
-{
-    /* Read user input using custom my_getline function */
-    char *line = my_getline();
-
-    if (line == NULL)
-    {
-        /* End of input or error, exit gracefully */
-        exit(EXIT_SUCCESS);
-    }
-
-    /* Copy the line into the input buffer */
-    strncpy(input, line, MAX_INPUT_LENGTH);
-    input[MAX_INPUT_LENGTH - 1] = '\0';
-
-    /* Calculate the length of the input */
-    *input_length = strlen(input);
-
-    /* Free the allocated memory for the line */
-    free(line);
-
-    return (0);
-}
-
-=======
 int readInput(char input[], size_t *input_length)
 {
 	/* Read user input using custom my_getline function */
@@ -76,59 +47,12 @@ int readInput(char input[], size_t *input_length)
 
 
 
->>>>>>> 7b438768a8b9bca30c718c10910b47dd95cab9e7
 /**
  * my_getline - Custom getline function for reading user input.
  *
  * Return: A dynamically allocated line of input, or NULL on error or end of
  * input. It is the caller's responsibility to free the allocated memory.
  */
-<<<<<<< HEAD
-
-char *my_getline(void)
-{
-    static char buffer[BUFFER_SIZE];
-    char *line = NULL;
-    ssize_t line_size = 0;
-
-    while (1)
-    {
-        ssize_t bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE), i;
-
-        if (bytes_read <= 0)
-        {
-            if (line_size > 0)
-                return (line);
-            return (NULL); /* End of input */
-        }
-        for (i = 0; i < bytes_read; i++)
-        {
-            char current_char = buffer[i];
-
-            if (current_char == '\n')
-            {
-                line = realloc(line, line_size + 1);
-                if (!line)
-                {
-                    perror("Memory allocation error");
-                    exit(EXIT_FAILURE);
-                }
-                line[line_size] = '\0';
-                return (line);
-            }
-            line = realloc(line, line_size + 2);
-            if (!line)
-            {
-                perror("Memory allocation error");
-                exit(EXIT_FAILURE);
-            }
-            line[line_size] = current_char;
-            line[line_size + 1] = '\0';
-            line_size++;
-        }
-    }
-}
-=======
 char *my_getline(void)
 {
 	static char buffer[BUFFER_SIZE];
@@ -174,4 +98,3 @@ char *my_getline(void)
 }
 
 
->>>>>>> 7b438768a8b9bca30c718c10910b47dd95cab9e7
